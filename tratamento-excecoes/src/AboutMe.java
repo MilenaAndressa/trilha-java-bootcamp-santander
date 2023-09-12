7c9d32d13486759c06f5a30a6a72a144873a2676
Milena Santos <milena.andressa_@outlook.com>
2023-09-12 11:43:54 -0300

    tratamento de exceções

---
+++ tratamento-excecoes/src/AboutMe.java
@@ -0,0 +1,40 @@
+import java.util.InputMismatchException;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class AboutMe {
+    public static void main(String[] args) {
+        try {
+            //criando o objeto scanner
+            Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+            System.out.println("Digite seu nome");
+            String nome = scanner.next();
+
+            System.out.println("Digite seu sobrenome");
+            String sobrenome = scanner.next();
+
+            System.out.println("Digite sua idade");
+            int idade = scanner.nextInt();
+
+            System.out.println("Digite sua altura");
+            double altura = scanner.nextDouble();
+
+
+            //imprimindo os dados obtidos pelo usuario
+            System.out.println("Olá, me chamo " + nome.toUpperCase() + " " + sobrenome.toUpperCase());
+            System.out.println("Tenho " + idade + " anos ");
+            System.out.println("Minha altura é " + altura + "cm ");
+            scanner.close();
+        }catch (InputMismatchException excecao){
+            System.err.println("Os campos idade e altura precisam ser numéricos.");
+        }
+
+    }
+}
+/* POSSÍVEIS ERROS QUE PODEM ACONTECER
+Não informar o nome e/ou sobrenome
+o valor da idade ter um caractere não número ou com formatação diferente
+* entrando com o número por extenso: INPUTMISMATCHEXCEPTION
+o valor da altura ter vírgula ao invés de ponto ou não ser numérico
+ */
---
+++ tratamento-excecoes/src/FormatadorCepExemplo.java
@@ -0,0 +1,16 @@
+public class FormatadorCepExemplo {
+    public static void main(String[] args) {
+        try {
+            String cepFormatado = formatarCep("2376506");
+            System.out.println(cepFormatado);
+        } catch (CepInvalidoException e) {
+            System.out.println("Cep falta informação");
+        }
+    }
+    static String formatarCep(String cep) throws CepInvalidoException{
+        if(cep.length() != 8)
+            throw new CepInvalidoException();
+
+        return "23.765-064";
+    }
+}
